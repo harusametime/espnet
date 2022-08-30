699e1a17486e28ca670e8d94681fa7b9b411096b
Masaki Samejima <samejim@amazon.co.jp>
2022-08-31 00:22:44 +0900

    Adjust to SageMaker

--- espnet2/bin/asr_train.py
+++ espnet2/bin/asr_train.py
@@ -16,7 +16,29 @@ def main(cmd=None):
                 > conf/train_asr.yaml
         % python asr_train.py --config conf/train_asr.yaml
     """
-    ASRTask.main(cmd=cmd)
+    if os.path.exists('/opt/ml/input/config/hyperparameters.json'):
+
+        # Parse args for sagemaker; as it is hard to pass list directory,
+        # args are passed as str and parsed as str
+        import argparse
+        parser = argparse.ArgumentParser(description='SageMaker and Espnet args')
+        parser.add_argument('--cmd', type=str, default="")
+        args = parser.parse_args()
+        cmd = eval(args.cmd)
+        cmd.append("sagemaker")
+
+        # SageMaker downloads data in /opt/ml/input/, but espnet assumes the data
+        # in current directory. Create symblic link to /opt/ml/input/, which can be
+        # known from evnrionment variables
+        os.symlink(os.environ['SM_CHANNEL_DATA'], "data")
+        os.symlink(os.environ['SM_CHANNEL_EXP'], "exp")
+        os.symlink(os.environ['SM_CHANNEL_DUMP'], "dump")
+        os.symlink(os.environ['SM_CHANNEL_CONF'], "conf")
+
+        ASRTask.main(cmd=cmd)
+
+    else:
+        ASRTask.main(cmd=cmd)
 
 
 if __name__ == "__main__":
