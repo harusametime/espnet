38a2c2945636965d1e3476c9c04cab445c343b87
Masaki Samejima <samejim@amazon.co.jp>
2022-10-07 21:12:23 +0900

    receive sagemaker hyperparameters

--- espnet2/bin/gan_tts_train.py
+++ espnet2/bin/gan_tts_train.py
@@ -17,11 +17,31 @@ def main(cmd=None):
         % python gan_tts_train.py --config conf/train.yaml
     """
 
-    if os.path.exists('/opt/ml/input/config/hyperparameters.json'):
-        import argparse
-        parser = argparse.ArgumentParser(description='Process some integers.')
-        parser.add_argument('--cmd', type=str, default="")
-        args = parser.parse_args()
+    sagemaker_hp_json_path  = '/opt/ml/input/config/hyperparameters.json'
+    if os.path.exists(sagemaker_hp_json_path):
+
+        # Parse args for sagemaker; as it is hard to pass list directory,
+        # args are passed as str and parsed as str
+        import json
+        with open(sagemaker_hp_json_path) as f:
+            hp_json = json.load(f)
+        cmd = eval(hp_json['cmd'])
+
+
+        # SageMaker downloads data in /opt/ml/input/, but espnet assumes the data
+        # in current directory. Create symblic link to /opt/ml/input/, which can be
+        # known from evnrionment variables
+        try:
+            os.symlink(os.environ['SM_CHANNEL_DATA'], "data")
+            os.symlink(os.environ['SM_CHANNEL_EXP'], "exp")
+            os.symlink(os.environ['SM_CHANNEL_DUMP'], "dump")
+            os.symlink(os.environ['SM_CHANNEL_CONF'], "conf")
+            os.symlink(os.environ['SM_CHANNEL_DOWNLOADS'], "downloads")
+        except FileExistsError:
+            print("[Info] Symbolic link to data directory has been created before. This process continues by re-using the link.")
+        except:
+            raise
+
         GANTTSTask.main(cmd=cmd)
     else:
         GANTTSTask.main(cmd=cmd)
